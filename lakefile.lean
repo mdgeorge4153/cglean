60cf50dc7d09d0b68e829653ba5c7f23485397ce
Michael George <michael@certora.com>
2024-05-22 10:41:11 -0400

    updated to lean 4.8

--- lakefile.lean
+++ lakefile.lean
@@ -6,9 +6,12 @@ package cglean
 @[default_target]
 lean_lib CGLean
 
+require mathlib from git
+    "https://github.com/leanprover-community/mathlib4" @ "v4.8.0-rc1"
+
 require leancolls from git
-  "https://github.com/JamesGallicchio/LeanColls.git" @ "main"
+  "https://github.com/mdgeorge4153/LeanColls.git" @ "lean-4.8"
 
 require ray from git
-  "https://github.com/girving/ray.git" @ "main"
+  "https://github.com/girving/ray.git" @ "lean-4.8"
 
