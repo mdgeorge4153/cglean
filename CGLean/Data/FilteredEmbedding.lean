01af9c79d6abc234e0aa4982b4b9d45adcb94131
Michael George <michael@certora.com>
2024-05-30 17:06:34 -0400

    progress towards BEq

--- CGLean/Data/FilteredEmbedding.lean
+++ CGLean/Data/FilteredEmbedding.lean
@@ -174,13 +174,148 @@ instance: Mul (FilteredReal f) where
 
 end Mul
 
+/- TODO: this section only depends on intervals, migrate it somewhere -/
+
+namespace Interval
+
+/--
+Returns `some o` if `x` is entirely to the left or entirely to the right of `y`
+-/
+def compare? (x y : Interval) : Option Ordering :=
+  if x = nan ∨ y = nan then none
+  else if x.lo > y.hi then some .gt
+  else if x.hi < y.lo then some .lt
+  else if x.lo = y.hi ∧ x.hi = y.lo then some .eq
+  else none
+
+/--
+If `compare?` returns `some o`, then comparing any element of the intervals
+must return `o`
+-/
+
+theorem compare_of_compare?_approx:
+  ∀ (i₁ i₂ : Interval), compare? i₁ i₂ = some o →
+    ∀ x₁ ∈ approx i₁, ∀ x₂ ∈ approx i₂, compare x₁ x₂ = o := by
+      -- TODO: this proof can probably be automated a lot better
+      intros i₁ i₂ cmp x₁ in₁ x₂ in₂
+      unfold compare? at cmp
+      split_ifs at cmp with hnan hgt hlt heq
+        <;> simp at hnan
+        <;> cases hnan
+        <;> rw [Option.some.injEq] at cmp
+        <;> rw [←cmp]
+      . have lt: x₂ < x₁ := by calc
+          x₂ ≤ i₂.hi.val := by apply le_hi <;> assumption
+           _ < i₁.lo.val := by simpa using hgt
+           _ ≤ x₁        := by apply lo_le <;> assumption
+        simpa [compare_gt_iff_gt] using lt
+      . have lt: x₁ < x₂ := by calc
+          x₁ ≤ i₁.hi.val := by apply le_hi <;> assumption
+           _ < i₂.lo.val := by simpa using hlt
+           _ ≤ x₂        := by apply lo_le <;> assumption
+        simpa [compare_lt_iff_lt] using lt
+      . have le₁₂: x₁ ≤ x₂ := by calc
+          x₁ ≤ i₁.hi.val := by apply le_hi <;> assumption
+           _ = i₂.lo.val := by rw [heq.2]
+           _ ≤ x₂        := by apply lo_le <;> assumption
+        have le₂₁: x₂ ≤ x₁ := by calc
+          x₂ ≤ i₂.hi.val := by apply le_hi <;> assumption
+           _ = i₁.lo.val := by rw [heq.1]
+           _ ≤ x₁        := by apply lo_le <;> assumption
+        have eq: x₁ = x₂ := by exact eq_of_le_of_le le₁₂ le₂₁
+        simpa [compare_eq_iff_eq]
+
+lemma mem_approx_nan : ∀ (x : ℝ) (i : Interval), i = nan → x ∈ approx i := by simp
+
+lemma exists_in [Membership α β] {p : α → Prop} {y : β} (x : α) (h: x ∈ y) (h': p x) : ∃ x ∈ y, p x := by exists x
+
+/--
+`compare?` only returns `none` if the intervals are incomparable. This is not
+necessary for correctness of FilteredReal, but ensures that we don't evaluate
+thunks unnecessarily.
+-/
+theorem compare?_complete:
+  ∀ (x y : Interval), compare? x y = none →
+    ∃ x₁ ∈ approx x, ∃ x₂ ∈ approx x, ∃ y₁ ∈ approx y, ∃ y₂ ∈ approx y,
+      compare x₁ y₁ ≠ compare x₂ y₂ := by
+        unfold compare?
+        intros x y cmp
+        split_ifs at cmp with nan
+        . cases nan
+          . -- x = nan
+            let x₁ := y.lo.val - 1
+            let x₂ := y.hi.val + 1
+            let y₁ := y.lo.val
+            let y₂ := y.hi.val
+
+            apply exists_in x₁ (by apply mem_approx_nan; assumption)
+            apply exists_in x₂ (by apply mem_approx_nan; assumption)
+            apply exists_in y₁ (by exact lo_mem)
+            apply exists_in y₂ (by exact hi_mem)
+
+            have hlt: compare x₁ y₁ = .lt := by rw [compare_lt_iff_lt]; simp [x₁, y₁]
+            have hgt: compare x₂ y₂ = .gt := by rw [compare_gt_iff_gt]; simp [x₂, y₂]
+
+            simp [hlt, hgt]
+          . -- y = nan (TODO: this repeats the x = nan case; abstract it out)
+            let x₁ := x.lo.val
+            let x₂ := x.hi.val
+            let y₁ := x.lo.val - 1
+            let y₂ := x.hi.val + 1
+
+            apply exists_in x₁ (by exact lo_mem)
+            apply exists_in x₂ (by exact hi_mem)
+            apply exists_in y₁ (by apply mem_approx_nan; assumption)
+            apply exists_in y₂ (by apply mem_approx_nan; assumption)
+
+            have hlt: compare x₁ y₁ = .gt := by rw [compare_gt_iff_gt]; simp [x₁, y₁]
+            have hgt: compare x₂ y₂ = .lt := by rw [compare_lt_iff_lt]; simp [x₂, y₂]
+
+            simp [hlt, hgt]
+
+        . sorry
+
+end Interval
+
+
 /-! ## Equality ---------------------------------------------------------------/
 
-instance [BEq α] (f : α → ℝ): BEq (FilteredEmbedding f) where
-  beq x y :=
-    if x.range.i.hi < y.range.i.lo then false
-    else if x.range.i.lo > y.range.i.hi then false
-    else x.value.get == y.value.get
+section BEq
+
+variable [BEq α] (f : α → ℝ)
+
+instance: BEq (FilteredEmbedding f) where
+  beq x y := match Interval.compare? x.range.i y.range.i with
+    | some .eq => true
+    | some .lt | some .gt => false
+    | none => x.value.get == y.value.get
+
+variable [LawfulBEq α]
+
+theorem eqv_of_beq: ∀ (x y : FilteredEmbedding f), x == y → x.toReal = y.toReal := by
+  intros x y h
+  simp only [BEq.beq] at h
+  split at h
+  . simp [←compare_eq_iff_eq, Interval.compare_of_compare?_approx _ _ (by assumption), Around.mem]
+  . contradiction
+  . contradiction
+  . simp at h; simp [h]
+
+theorem beq_of_eqv: ∀ (x y : FilteredEmbedding f), eqv f x y → x == y := by
+  simp only [BEq.beq]
+  intros x y h
+  split
+  . rfl
+  . have lt: x.toReal < y.toReal := by
+      simp [←compare_lt_iff_lt, Interval.compare_of_compare?_approx _ _ (by assumption), Around.mem]
+    simp at h
+    simp [h] at lt
+  . have gt: x.toReal > y.toReal := by
+      simp [←compare_gt_iff_gt, Interval.compare_of_compare?_approx _ _ (by assumption), Around.mem]
+    simp at h
+    simp [h] at gt
+  . simpa using h
+
 
 -- TODO: implement BEq and/or DecidableEq
 -- TODO: comparisons
